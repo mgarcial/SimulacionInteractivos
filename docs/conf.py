80c552810c7569a88829749334d93d283054d53f
Juan Franco WSL <juanf.franco@upb.edu.co>
2022-12-19 19:19:59 -0500

    change latex doc title

--- docs/conf.py
+++ docs/conf.py
@@ -128,7 +128,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'simulacionInteractivosDocs.tex', 'simulacionInteractivosDocs Documentation',
+    (master_doc, 'simulacionInteractivosDocs.tex', 'Simulación para sistemas interactivos',
      'Juan Franco', 'manual'),
 ]
 
@@ -138,7 +138,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'simulacionInteractivosDocs', 'simulacionInteractivosDocs Documentation',
+    (master_doc, 'simulacionInteractivosDocs', 'Simulación para sistemas interactivos',
      [author], 1)
 ]
 
@@ -149,7 +149,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'simulacionInteractivosDocs', 'simulacionInteractivosDocs Documentation',
+    (master_doc, 'simulacionInteractivosDocs', 'Simulación para sistemas interactivos',
      author, 'simulacionInteractivosDocs', 'One line description of project.',
      'Miscellaneous'),
 ]
